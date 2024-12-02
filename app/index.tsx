403f30aa6b1cfebef6836913fa1f24fa627b2b16
MosElsh <70161901+test1234d5x2@users.noreply.github.com>
2024-12-02 17:20:23 +0000

    A Start On The UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import Page from "./components/page";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Wallet Guru</Text>
+      <Page></Page>
     </View>
   );
 }
