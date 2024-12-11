94818f8ecba09e3367e23a4d9ee28d07f28b9f49
MosElsh <70161901+test1234d5x2@users.noreply.github.com>
2024-12-11 11:02:04 +0000

    Date Picker Functionaility Modifications For Android

--- components/formComponents/inputFields/dateInputField.tsx
+++ components/formComponents/inputFields/dateInputField.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import DateTimePicker from '@react-native-community/datetimepicker';
-
+import { Text, View } from "react-native";
 
 interface DateInputFieldProps {
     setDate: (text: string) => void
@@ -10,19 +10,30 @@ interface DateInputFieldProps {
 export default function DateInputField(props: DateInputFieldProps) {
     
     const [dateDisplayValue, setDate] = useState<Date>(new Date())
+    const [show, setShow] = useState<boolean>(false)
 
     const handleDateChange = (event: any, selectedDate?: Date) => {
         const currentDate = selectedDate || dateDisplayValue
         setDate(currentDate)
         props.setDate(currentDate.toString())
+        setShow(false)
+        
+    }
+
+    const showPicker = () => {
+        setShow(true)
     }
 
     return (
-        <DateTimePicker
+        <View style={{paddingVertical: 20, paddingLeft: 15, borderWidth: 1, borderColor: "#ccc", borderRadius: 10}}>
+            <Text onPress={showPicker}>Date: {dateDisplayValue.toDateString()}</Text>
+            {show && <DateTimePicker
                 value={dateDisplayValue}
                 onChange={handleDateChange}
                 mode="date" 
                 display="default"
-            />
+            />}
+        </View>
+        
     )
 }
