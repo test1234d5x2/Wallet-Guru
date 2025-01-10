8406473f68acbbaddd3e6ee121f6eb554ab1d908
MosElsh <70161901+test1234d5x2@users.noreply.github.com>
2025-01-10 10:48:14 +0000

    Method For Calculating Monthly Transactions Total

---
+++ utils/filterTransactionByMonth.ts
@@ -0,0 +1,12 @@
+import Transaction from "@/models/Transaction";
+
+export default function filterTransactionByMonth(transactions: Transaction[], month: Date): Transaction[] {
+    return transactions.filter((transaction) => {
+        const transactionMonth = transaction.date.getMonth();
+        const transactionYear = transaction.date.getFullYear();
+        const targetMonth = month.getMonth();
+        const targetYear = month.getFullYear();
+
+        return transactionMonth === targetMonth && transactionYear === targetYear;
+    });
+}
