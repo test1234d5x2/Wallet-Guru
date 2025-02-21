e37b74de3deac96bb696242fb03f7a9f3d821f25
test1234d5x2 <70161901+test1234d5x2@users.noreply.github.com>
2025-02-21 10:38:29 +0000

    Fixed TSError

--- middlewareAPI/src/routes/User.ts
+++ middlewareAPI/src/routes/User.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { create, login, update, remove } from "../controllers/User";
+import { create, login, remove } from "../controllers/User";
 
 const router = Router();
 
 router.post("/", create);
 router.post("/login", login);
-router.put("/:id", update);
 router.delete("/:id", remove);
 
 export default router;
