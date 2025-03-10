84cfa1d2e97aed1efa2de512ed0e3e704b65a40a
test1234d5x2 <70161901+test1234d5x2@users.noreply.github.com>
2025-03-10 21:04:39 +0000

    BREAKING CHANGES PART 1

--- middlewareAPI/src/app.ts
+++ middlewareAPI/src/app.ts
@@ -1,43 +1,42 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import expenseRoutes from "./routes/Expense";
-import userRoutes from "./routes/User";
-import expenseCategoryRoutes from "./routes/ExpenseCategory";
-import incomeRoutes from "./routes/Income";
-import goalRoutes from "./routes/Goal";
-import recurringExpenseRouter from "./routes/RecurringExpense";
-import recurringIncomeRouter from "./routes/RecurringIncome";
-import verifyTokenRouter from "./routes/token";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import expenseRoutes from './routes/Expense'
+import userRoutes from './routes/User'
+import expenseCategoryRoutes from './routes/ExpenseCategory'
+import incomeRoutes from './routes/Income'
+import goalRoutes from './routes/Goal'
+import recurringExpenseRouter from './routes/RecurringExpense'
+import recurringIncomeRouter from './routes/RecurringIncome'
+import verifyTokenRouter from './routes/token'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-let tempPort = 5005;
+let tempPort = 5005
 if (process.env.PORT) {
-    tempPort = parseInt(process.env.PORT);
+    tempPort = parseInt(process.env.PORT)
 }
 
-let tempIP = 'localhost';
+let tempIP = 'localhost'
 if (process.env.IP) { tempIP = process.env.IP }
 
-const IP = tempIP;
-const PORT = tempPort;
+const IP = tempIP
+const PORT = tempPort
 
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
-app.use(express.json());
+app.use('/api/expenses', expenseRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/expense-categories', expenseCategoryRoutes)
+app.use('/api/incomes', incomeRoutes)
+app.use('/api/goals', goalRoutes)
+app.use('/api/recurring-expenses', recurringExpenseRouter)
+app.use('/api/recurring-incomes', recurringIncomeRouter)
+app.use('/api/verify-token', verifyTokenRouter)
 
-app.use("/api/expenses", expenseRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/expense-categories", expenseCategoryRoutes);
-app.use("/api/incomes", incomeRoutes);
-app.use("/api/goals", goalRoutes);
-app.use("/api/recurring-expenses", recurringExpenseRouter);
-app.use("/api/recurring-incomes", recurringIncomeRouter)
-app.use("/api/verify-token", verifyTokenRouter)
+app.listen(PORT, IP, () => console.log(`Server running on https://${IP}:${PORT}`))
 
-app.listen(PORT, IP, () => console.log(`Server running on  https://${IP}:${PORT}`));
-
-export default app;
+export default app
