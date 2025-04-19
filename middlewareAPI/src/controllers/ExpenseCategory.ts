1ef746668fa1ffd2af2f31a302865ade9000d1d5
test1234d5x2 <70161901+test1234d5x2@users.noreply.github.com>
2025-04-19 11:19:54 +0100

    Log Statement Removed

--- middlewareAPI/src/controllers/ExpenseCategory.ts
+++ middlewareAPI/src/controllers/ExpenseCategory.ts
@@ -106,7 +106,6 @@ export const listByUser: RequestHandler = async (req, res) => {
     const expenseCategoryService = registry.expenseCategoryService
 
     const categories = await expenseCategoryService.getAllCategoriesByUser(userID)
-    console.log(categories)
     res.status(200).json({ categories })
 }
 
